444ab14b43ab16b3cfd054d5dea5dd0bfa940dae
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-10-15 10:21:37 +0100

    refactory done

--- Home/home.js
+++ Home/home.js
@@ -1,1 +1,108 @@
 
+function tayesh(){
+    var url = "file:///C:/Users/INFOKOM/Desktop/Quiz-Game-/Game/game.html";
+$(location).attr('href',url);
+}
+
+/////////////////
+$("ul").hide()
+function tayesh(){
+    $("ul").show(1000)
+    $("h1").hide(1000)
+    $("#BOT").hide(1000)
+
+}
+
+
+
+var start = 5;
+function myCounter() {
+document.getElementById("demo").innerHTML = --start;
+}
+
+$(".img2").hide()
+$(".img3").hide()
+$(".img4").hide()
+
+/////////////////
+$("#USA").hide()
+$("#Russia").hide()
+$("#canada").hide()
+$("#france").hide()
+//////////////////////
+
+$("#Maldives").hide()
+$(".img3").hide()
+$("#Seychelles").hide()
+$("#eswatini").hide()
+$("#Maurice").hide()
+$(".img4").hide()
+$("#Philippines").hide()
+$("#uruguay").hide()
+$("#germany").hide()
+$("#argentina").hide()
+
+function truee(){
+         $("#palastin").hide(1000)
+         $(".img1").hide(1000)
+         $("#orden").hide(1000)
+         $("#egypt").hide(1000)
+    $("#emirates").hide(1000)
+    $(".img2").show(1000)
+    $("#USA").show(1000)
+    $("#france").show(1000)
+    $("#canada").show(1000)
+    $("#Russia").show(1000)
+
+    }
+
+$(".img3").hide()
+
+    function truee1(){
+        $("#france").hide(1000)
+        $(".img2").hide(1000)
+        $("#canada").hide(1000)
+        $("#USA").hide(1000)
+        $("#Russia").hide(1000)
+        $(".img3").show(1000)
+        $("#Maldives").show(1000)
+        $("#Seychelles").show(1000)
+        $("#Maurice").show(1000)
+        $("#eswatini").show(1000)
+
+
+
+    }
+   
+    function truee3(){
+        $("#Maldives").hide(1000)
+        $(".img3").hide(1000)
+        $("#Seychelles").hide(1000)
+        $("#eswatini").hide(1000)
+        $("#Maurice").hide(1000)
+        $(".img4").show(1000)
+        $("#Philippines").show(1000)
+        $("#uruguay").show(1000)
+        $("#germany").show(1000)
+        $("#argentina").show(1000)
+        
+    }
+    $(".img5").hide()
+ $(".danimarca").hide()
+ $(".dominican").hide()
+ $(".australia").hide()
+ $(".espain").hide()
+
+    function truee4(){
+        $(".img4").hide(1000)
+        $("#Philippines").hide(1000)
+        $("#uruguay").hide(1000)
+        $("#germany").hide(1000)
+        $("#argentina").hide(1000)
+        $(".img5").show(1000)
+        $(".danimarca").show(1000)
+        $(".dominican").show(1000)
+        $(".australia").show(1000)
+        $(".espain").show(1000)
+    }
+   
